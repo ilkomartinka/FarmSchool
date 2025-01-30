6a4b2618a37420e124c1a6930d62692186878fe0
ilko <ilkomartina12@gmail.com>
2025-01-30 10:42:36 +0100

    Add some logic for work with flowers

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class Farm {
+    private int counofFlowers;
+    private int countOfSmallAnimals;
+    private int countOfLargeAnimals;
+    private ArrayList<Flower> flowers;
+    private ArrayList<Animal> animals;
+    private double landArea;
 
+    public Farm(int counofFlowers, int countOfSmallAnimals, int countOfLargeAnimals, ArrayList<Flower> flowers, ArrayList<Animal> animals, int landArea) {
+        this.counofFlowers = 5;
+        this.countOfSmallAnimals = 10;
+        this.countOfLargeAnimals = 10;
+        this.flowers = new ArrayList<>();
+        this.animals = new ArrayList<>();
+        this.landArea = 100;
+    }
 
+    public void addFlower(Flower flower) {
+        if (flower.getNeededArea() < landArea && counofFlowers < 5) {
+            flowers.add(flower);
+            landArea =- flower.getNeededArea();
+        } else {
+            System.out.println("Error");
+        }
+    }
 
 }
